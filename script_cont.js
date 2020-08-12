df1b023f13f8ae12c26c54237e4d673f2de3a181
Leandro Fogaça <fogaca.lf@gmail.com>
2020-08-12 07:14:14 -0300

    Incio dos calculos

--- script_cont.js
+++ script_cont.js
@@ -1,1 +1,16 @@
+const body = document.querySelector('body')
 var data = sessionStorage.getItem('data')
+
+var now = new Date()
+
+var diff = data - now
+
+var d, h, m, s 
+
+
+d = Math.round((diff/(1000*60*60*24)*(-1)))
+h = Math.round((diff/(1000*60*60)*(-1)))
+m = Math.round((diff/(1000*60)*(-1)))
+s = Math.round((diff/(1000)*(-1)))
+
+
