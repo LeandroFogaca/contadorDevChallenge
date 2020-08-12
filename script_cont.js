555a6a7820c511ad7607418c9781e4d8f68727fb
Leandro Fogaça <fogaca.lf@gmail.com>
2020-08-12 20:50:58 -0300

    Lógica finalizada

--- script_cont.js
+++ script_cont.js
@@ -1,16 +1,33 @@
 const body = document.querySelector('body')
-var data = sessionStorage.getItem('data')
+const button = document.querySelector('#reiniciar')
+const Data = sessionStorage.getItem('data')
+const Dias = document.querySelector("#d")
+const Horas = document.querySelector("#h")
+const Min = document.querySelector("#m")
+const Sec = document.querySelector("#s")
 
+var data = new Date(parseInt(Data))
 var now = new Date()
 
-var diff = data - now
+var diff = Data - now
 
-var d, h, m, s 
+var d, h, m, s
+
+d = Math.floor((diff/(1000*60*60*24)*(-1)))
+
+h = ((data.getHours() - 21) - now.getHours())*(-1)
+
+m = (data.getMinutes() - now.getMinutes())*(-1)
+
+s = (data.getSeconds() - now.getSeconds())*(-1)
 
 
-d = Math.round((diff/(1000*60*60*24)*(-1)))
-h = Math.round((diff/(1000*60*60)*(-1)))
-m = Math.round((diff/(1000*60)*(-1)))
-s = Math.round((diff/(1000)*(-1)))
 
+Dias.innerHTML = d
+Horas.innerHTML = h
+Min.innerHTML = m
+Sec.innerHTML = s
 
+button.addEventListener("click", function () {    
+        window.location.href = "./index.html";
+});
